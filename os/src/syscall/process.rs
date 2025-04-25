85af357e8fe82dd1b65f94ec2c99b65f9de0b976
jinyu <jinyuchen_derick@outlook.com>
2025-04-25 12:31:45 +0800

    feat: trace id 0 and 1

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -39,7 +39,14 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 }
 
 // TODO: implement the syscall
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
+pub fn sys_trace(trace_request: usize, id: usize, data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match trace_request {
+        0 => unsafe { (id as *const u8).read_volatile().into() },
+        1 => {
+            unsafe { (id as *mut u8).write_volatile(data as u8) };
+            0
+        }
+        _ => -1
+    }
 }
